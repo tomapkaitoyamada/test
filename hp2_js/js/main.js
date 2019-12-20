95894833aaa8585a527b2b93e2ac26e2570cb370
tomapkaitoyamada <tomap.kaitoyamada@gmail.com>
2019-12-20 16:24:01 +0900

    hp

---
+++ hp2_js/js/main.js
@@ -0,0 +1,16 @@
+
+$(function () {
+    $(window).scroll(function () {
+        $('.fadein').each(function () {
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 100) {
+                $(this).addClass('scrollIn');
+            }
+        });
+    });
+});
+
+
+
